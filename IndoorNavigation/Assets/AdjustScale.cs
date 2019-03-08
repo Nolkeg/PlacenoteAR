f3a9ada50d057e07e24fb1a1ed731602d86257ee
Jiraphat <be154864ef@gmail.com>
2019-03-08 23:24:53 +0700

    Fixed map selection bug

---
+++ IndoorNavigation/Assets/AdjustScale.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdjustScale : MonoBehaviour
+{
+
+    void Start()
+    {
+		transform.localScale = new Vector3(1, 1, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
