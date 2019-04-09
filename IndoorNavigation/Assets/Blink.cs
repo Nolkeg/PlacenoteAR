514ba9f2afacb5dec8198dd622d4be34199ff4d6
Nolkeg <46750330+Nolkeg@users.noreply.github.com>
2019-04-09 10:59:44 +0700

    Update Prefab

---
+++ IndoorNavigation/Assets/Blink.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Blink : MonoBehaviour
+{
+	Renderer image;
+	[SerializeField] float timeDelay = 1;
+	
+	bool blinkUp;
+
+	private void Start()
+	{
+		image = GetComponent<Renderer>();
+		StartBlinking();
+	}
+
+	IEnumerator Blinking()
+	{
+		while (true)
+		{
+			switch (blinkUp)
+			{
+				case false:
+					image.material.DOFade(1, timeDelay);
+					yield return new WaitForSeconds(timeDelay);
+					blinkUp = true;
+					break;
+
+				case true:
+					image.material.DOFade(0.5f, timeDelay);
+					yield return new WaitForSeconds(timeDelay);
+					blinkUp = false;
+					break;
+			}
+		}
+	}
+
+	void StartBlinking()
+	{
+		StopAllCoroutines();
+		StartCoroutine(Blinking());
+	}
+
+	void StopBlinking()
+	{
+		StopAllCoroutines();
+	}
+}
